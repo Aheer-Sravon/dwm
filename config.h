739a96497174610b1269a6bbfff415c21426af4c
Srabon <totalblank@yahoo.com>
2026-06-30 03:52:12 +0600

    Some changes

--- config.h
+++ config.h
@@ -5,20 +5,21 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       =   "monospace:size=14";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+
+static const char col_gray1[]       = "#101010";
+static const char col_gray2[]       = "#2c2c2c";
+static const char col_gray3[]       = "#eeeeee";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray3, col_gray2,  col_cyan  },
 };
 
 /* tagging */
@@ -61,22 +62,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {
-	"dmenu_run",
-	"-m",
-	dmenumon,
-	"-fn",
-	dmenufont,
-	"-nb",
-	col_gray1,
-	"-nf",
-	col_gray3,
-	"-sb",
-	col_cyan,
-	"-sf",
-	col_gray4,
-	NULL
-};
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+
 static const char *termcmd[]  = { "kitty", NULL };
 
 static const char *brightup[]   = { "brightnessctl", "set", "2%+", NULL };
